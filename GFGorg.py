2938ce5cbfa4613d0ecebe781b0d3a56aaa548aa
Aleksandr Efimov <aleksander.v.efimov@gmail.com>
2025-04-16 17:59:37 +0300

    yac

--- GFGorg.py
+++ GFGorg.py
@@ -4,6 +4,27 @@
 """ Multi-line comment used
 print("Python Comments") """
 
-name = input("Enter your name: ")
-print(f"Hello, {name}! You look good!")
-print("Hello, "+name+"! You look good!")
+# name = input("Enter your name: ")
+# print(f"Hello, {name}! You look good!")
+# print("Hello, "+name+"! You look good!")
+
+# Prompting the user for input
+while True:
+    try:
+        age_input = int(input("Enter your age: "))
+        break
+    except ValueError:
+        print("Please, enter your age in numbers")
+
+# Converting the input to an integer
+age = int(age_input)
+
+# Checking conditions based on user input
+if age < 0:
+    print("Please enter a valid age.")
+elif age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
