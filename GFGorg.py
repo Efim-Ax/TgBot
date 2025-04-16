7dfa2e7fa7cec39f2a517bc26d0844fcecea40a2
Aleksandr Efimov <aleksander.v.efimov@gmail.com>
2025-04-16 17:16:29 +0300

    Completed the first exercise

---
+++ GFGorg.py
@@ -0,0 +1,9 @@
+# I am single line comment
+"I am single line comment too"
+
+""" Multi-line comment used
+print("Python Comments") """
+
+name = input("Enter your name: ")
+print(f"Hello, {name}! You look good!")
+print("Hello, "+name+"! You look good!")
